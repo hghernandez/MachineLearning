6d50d2c2c28a011adf570d26396d97efbbde421f
hghernandez <hernanghernandez@gmail.com>
2022-11-09 09:06:22 -0300

    actualizo files

--- R/RF Titanic/script.R
+++ R/RF Titanic/script.R
@@ -14,9 +14,15 @@ library(dplyr)
 
 #Cargo el dataset
 
+
 ruta <- "C:/Users/hernan.hernandez/Documents/Documents/MachineLearning/python/Random Forest Clasificador/Data/"
 
+
 titanic <- readr::read_csv(paste0(ruta,"titanic.csv"))
+
+ruta_pc_personal <- "C:/Users/usuario/Documents/MachineLearning/python/Random Forest Clasificador/Data/" 
+
+titanic <- readr::read_csv(paste0(ruta_pc_personal,"titanic.csv"))
 
 #Analisis exploratorio
 
@@ -46,12 +52,13 @@ sapply(titanic, function(x) sum(is.na(x)))
 
 #Creo dos variables
 ## Viaja en fiamilia
-## Titulo
+## Cambio survived a character
 
 titanic <- titanic %>%
   mutate(EnFamilia = case_when(SibSp + Parch > 0 ~ 1,
                                TRUE ~ 0)) %>%
-  select(-c(SibSp,Parch))
+  select(-c(SibSp,Parch)) %>%
+  mutate(Survived = as.character(Survived))
 
 ##%######################################################%##
 #                                                          #
@@ -66,7 +73,7 @@ titanic %>%
 
 titanic_num <- titanic %>% select_if(is.numeric)
 
-mat_cor <- dplyr::select(titanic_num,-Survived) %>% cor(method="pearson",use = "complete.obs") %>% round(digits=2) 
+mat_cor <- titanic_num %>% cor(method="pearson",use = "complete.obs") %>% round(digits=2) 
 
 library(corrplot)
 
@@ -81,5 +88,70 @@ corrplot(mat_cor, type="upper", order="hclust", tl.col="black", tl.srt=45)
 
 titanic_nom <- titanic %>% select_if(is.character)
 
-crosstable::crosstable(titanic_nom,cols = Sex,by = Embarked,
-                       funs=mean, test=TRUE)
+options(scipen = 1000000)
+
+
+survived_sex <- table(titanic_nom$Survived,titanic_nom$Sex)
+
+chisq.test(survived_sex)
+
+survived_emb <- table(titanic_nom$Survived,titanic_nom$Embarked)
+
+
+chisq.test(survived_emb)
+
+
+#Con un valor de p por debajo de 0.001 se rechaza la H0 de independencia
+#por lo que tanto embarked como sexo estan relacionadas significativamente
+# con survived
+
+##%######################################################%##
+#                                                          #
+####                  Preprocesamiento                  ####
+#                                                          #
+##%######################################################%##
+
+
+#Dividimos el dataset
+
+#Dividimos el dataset ----
+
+
+titanic <- titanic[,-c(4,7,8,9)]
+
+set.seed(123)
+
+split <- initial_split(titanic,strata = Survived)
+
+train <- training(split)
+
+test <- testing(split)
+
+
+
+
+#armo la receta
+## inputo los NA de las variables numericas con el valor de la media
+## inputo los NA de las variables categoricas con el valor más frecuente
+## creo variables dummy para las categoricas
+
+names(titanic)
+
+sapply(titanic, function(x) sum(is.na(x)))
+
+titanic_rec <- recipe(Survived ~ ., data = train) %>%
+  update_role(PassengerId, new_role = "ID") %>%
+  step_dummy(all_nominal(), -all_outcomes()) %>% #One hot encoding todas las nominales
+  step_impute_mean(Age,Pclass,EnFamilia)%>% #Imputo los NA con la media de Edad
+  step_impute_mode(Embarked,Sex) #Imputo los Na con la categoria más frecuente
+
+#Aprendo la receta
+
+train_prep <- prep(titanic_rec)
+
+
+rlang::last_error()
+
+juiced <- bake(train_prep,new_data = NULL)
+
+View(juiced)
